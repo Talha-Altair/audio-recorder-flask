77eaa5c6b8170f32d1baa2ba51c71dd25dfc4dcc
Talha-Altair <cooltalha27@gmail.com>
2021-08-31 10:09:04 +0530

    basefiles

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import os
 
 app = Flask(__name__)
 
+AUDIO_OUTPUT_FOLDER = 'static/audio'
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
 
     if request.method == "POST":
         f = request.files['audio_data']
-        with open('audio.wav', 'wb') as audio:
+        with open(f'{AUDIO_OUTPUT_FOLDER}/audio.wav', 'wb') as audio:
             f.save(audio)
         print('file uploaded successfully')
         return render_template('index.html', request="POST")
