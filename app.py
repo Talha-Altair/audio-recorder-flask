945f225e45522d390770af78f492b7ac4eec6920
Talha-Altair <cooltalha27@gmail.com>
2021-08-31 10:04:39 +0530

    basefiles

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import *
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+
+    if request.method == "POST":
+        f = request.files['audio_data']
+        with open('audio.wav', 'wb') as audio:
+            f.save(audio)
+        print('file uploaded successfully')
+        return render_template('index.html', request="POST")
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
